75aa1ab93fdc0149c28523dc9a436d6cc6db2e8f
Bhargav Nookala <nooknb@gmail.com>
2016-07-15 17:14:56 -0700

    Full authorization to config writing workflow

--- bin/gister.js
+++ bin/gister.js
@@ -2,7 +2,28 @@
 
 const github = require('github');
 const auth = require('../lib/auth.js');
+const config = require('../lib/config.js');
 
 let githubClient = new github({});
 
-auth.createAuthorization(githubClient).then((auth) => {console.log(auth)}, (error) => {console.log(error)});
+function onRejection (error) {
+    throw error;
+};
+
+function onConfigResolution (config) {
+    console.log(config);
+}
+
+function onMissingConfig (error) {
+    if (error && error.code === 'ENOENT') {
+        auth.createAuthorization(githubClient)
+            .then(config.createGisterConfig)
+            .then(onConfigResolution)
+            .catch(onRejection);
+    }
+}
+
+config.getGisterConfig().then(
+    onConfigResolution,
+    onMissingConfig
+).catch(onRejection);
